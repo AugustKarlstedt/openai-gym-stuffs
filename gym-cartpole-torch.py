061173773d7adcbef9b983deb49a90db1ea63396
August Karlstedt <august.karlstedt@gmail.com>
2019-07-30 19:02:48 -0700

    Fixes + cartpole model

--- gym-cartpole-torch.py
+++ gym-cartpole-torch.py
@@ -42,7 +42,7 @@ lr = 1e-2
 opt = optim.Adam(n.parameters(), lr=lr)
 batch_size = 4096
 epochs = 32
-render = False
+render = True
 
 for i in range(epochs):
     print(f"Epoch {i} of {epochs}: {i/epochs*100:.2f}% ", end="")
